9d9381c9f69cb390fd080574082b08be9508d3ca
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-08 00:04:28 +0300

    .

--- Lab6_2/Program.cs
+++ Lab6_2/Program.cs
@@ -10,6 +10,38 @@ namespace Lab6_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите предложение:");
+            string text = Console.ReadLine();
+            char space = ' ';
+            bool poly = true;
+            foreach (char s in text)
+            {
+                int indSpace = text.IndexOf(space);
+                if (indSpace > 0)
+                {
+
+                    text = text.Remove(indSpace,1);
+                }
+            }
+            text = text.ToLower();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != text[text.Length - i - 1])
+                {
+                    poly = false;
+                } 
+            } 
+            {
+                if (poly)
+                {
+                    Console.WriteLine("Предложение является полиндромом");
+                }
+                else
+                {
+                    Console.WriteLine("Предложение не является полиндромом");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
--- Lab6_3/Program.cs
+++ Lab6_3/Program.cs
@@ -10,6 +10,32 @@ namespace Lab6_3
     {
         static void Main(string[] args)
         {
+            char braceStart = '{';
+            char braceEnd = '}';
+            string text = Console.ReadLine();
+            string result = "";
+
+
+
+            while (text.Length > 0)
+            {
+                if ((text.IndexOf(braceStart) > 0) && (text.IndexOf(braceEnd) > 0))
+                {
+                    result += text.Substring(0, text.IndexOf(braceStart));
+                    text = text.Remove(0, text.IndexOf(braceEnd) + 1);
+                }
+                else
+                {
+                    result += text.Substring(0);
+                    text = text.Remove(0);
+                }
+
+
+            }
+
+
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
