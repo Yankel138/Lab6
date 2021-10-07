f402ab229b718a4fb78417c040a8bc73780ddefd
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-07 21:31:43 +0300

    Добавьте файлы проекта.

---
+++ Lab6_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            string longWord = "";
+           
+            foreach (string s in stringArray)
+            {
+
+                if (s.Length>longWord.Length)
+                   {
+                    longWord = s;
+                }
+            }
+            Console.WriteLine("Самое длинное слово: {0}", longWord);
+            Console.ReadKey();
+        }
+    }
+}
